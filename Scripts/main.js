134cf3cfd9e00123097292b49d7622d97726b6f0
Lunar-orbit1 <mgodron1@outlook.com>
2024-02-20 13:16:08 -0800

    h

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,5 +1,16 @@
 var currentTimeOffset = "-8"
-
+const images = [
+  {
+    Description: "A red sun sets behind a small branch",
+    ImageURL: "https://images.unsplash.com/photo-1693584591668-052350af2348?q=80&w=1467&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    UnsplashURL: "https://unsplash.com/photos/the-sun-is-setting-behind-a-tree-branch-zjUSwc9ACso"
+  },
+  {
+    Description: "A red sun sets behind a small branch",
+    ImageURL: "https://images.unsplash.com/photo-1693584591668-052350af2348?q=80&w=1467&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    UnsplashURL: "https://unsplash.com/photos/the-sun-is-setting-behind-a-tree-branch-zjUSwc9ACso"
+  },
+]
 function discord(){
     var copyText = "claym1x"
     navigator.clipboard.writeText(copyText).then(function() {
