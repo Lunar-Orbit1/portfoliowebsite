151b57f9b1342e6d4b52cfa1657a78157e33e632
Lunar-orbit1 <mgodron1@outlook.com>
2024-08-13 21:51:59 -0700

    v4.0

--- README.md
+++
@@ -1,4 +0,0 @@
-# My website
-
-Hi there! This is the public github repo to my website.
-Feel free to look at it, just don't steal the entire webpage <3
---
+++ copier.js
@@ -0,0 +1,33 @@
+const currentTimeOffset = "-8";
+document.addEventListener("DOMContentLoaded", function(){
+    const discordBtn = document.getElementById("dbutton")
+    discordBtn.addEventListener("click", function(){
+        navigator.clipboard.writeText("claym1x");
+        alert("Copied Discord username to clipboard")
+    })
+})
+
+function setTime(offset) {
+    var d = new Date();
+    var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    var nd = new Date(utc + (3600000*offset));
+  
+    var hrs = nd.getHours();
+    var mins = nd.getMinutes();
+    if (mins < 10){ //Jenk!!!!!!!
+        mins = `0${mins}`;
+    }
+  
+    var setD = `${hrs}:${mins} (UTC -8)`;
+    document.getElementById("clocktime").innerHTML = setD;
+  }
+
+
+  window.onload = function(){
+    console.log("Loaded site. Version 4.0")
+    setTime(currentTimeOffset)
+    setInterval(function() { 
+        setTime(currentTimeOffset)
+    }, 1000);
+  }
+  
